f392f227566ea3483c9bc21d2c0f94d8e7b1fae8
lucasbnunes <lucasbnunes23@gmail.com>
2021-12-10 10:24:21 -0300

    add load more button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
-import { AxiosResponse } from 'axios';
 
 interface Image {
   title: string;
@@ -22,7 +21,6 @@ export default function Home(): JSX.Element {
     data,
     isLoading,
     isError,
-    error,
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
@@ -30,7 +28,7 @@ export default function Home(): JSX.Element {
     getNextPageParam: lastPage => lastPage.data.after,
   });
 
-  async function fetchImages({ pageParam = null }: { pageParam?: string }) {
+  function fetchImages({ pageParam = null }: { pageParam?: string }) {
     return api.get('api/images', {
       params: {
         after: pageParam,
@@ -40,7 +38,10 @@ export default function Home(): JSX.Element {
 
   const formattedData: Image[] = useMemo(() => {
     if (data) {
-      return data.pages[0].data.data;
+      const formatted = data.pages.flatMap(page => page.data.data);
+      console.log(formatted);
+
+      return formatted;
     }
   }, [data]);
 
@@ -58,7 +59,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button mt={10} onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
